3528de28e2ac21c805193b18ede3aebaa166d11e
Sekrit <reaperanon415@gmail.com>
2021-09-22 22:18:13 +0300

    updated data path for new version of Synthesis

--- TrueUnleveledSkyrim/Config/Constants.cs
+++ TrueUnleveledSkyrim/Config/Constants.cs
@@ -10,6 +10,7 @@ namespace TrueUnleveledSkyrim.Config
     {
         // GitHub name of the patcher for the Data folder reference.
         public static string PatcherName { get; } = "True Unleveled Skyrim";
+        public static string DataPath { get; } = "Data/Skyrim Special Edition/";
 
         // List variant postfixes.
         public static string PostfixPart { get; } = "_TUS_";
@@ -17,16 +18,16 @@ namespace TrueUnleveledSkyrim.Config
         public static string StrongPostfix { get; } = "_TUS_Strong";
 
         // Json config paths.
-        public static string ArtifactKeysPath { get; } = "Data/" + PatcherName + "/artifactKeys.json";
-        public static string FollowersPath { get; } = "Data/" + PatcherName + "/customFollowers.json";
-        public static string ExcludedNPCsPath { get; } = "Data/" + PatcherName + "/excludedNPCs.json";
-        public static string ExcludedPerksPath { get; } = "Data/" + PatcherName + "/excludedPerks.json";
-        public static string NPCEDIDPath { get; } = "Data/" + PatcherName + "/NPCsByEDID.json";
-        public static string NPCFactionPath { get; } = "Data/" + PatcherName + "/NPCsByFaction.json";
-        public static string RaceModifiersPath { get; } = "Data/" + PatcherName + "/raceLevelModifiers.json";
-        public static string ZoneTyesEDIDPath { get; } = "Data/" + PatcherName + "/zoneTypesByEDID.json";
-        public static string ZoneTyesEDIDMLUPath { get; } = "Data/" + PatcherName + "/zoneTypesByEDIDMLU.json";
-        public static string ZoneTyesKeywordPath { get; } = "Data/" + PatcherName + "/zoneTypesByKeyword.json";
-        public static string ZoneTyesKeywordMLUPath { get; } = "Data/" + PatcherName + "/zoneTypesByKeywordMLU.json";
+        public static string ArtifactKeysPath { get; } = DataPath + PatcherName + "/artifactKeys.json";
+        public static string FollowersPath { get; } = DataPath + PatcherName + "/customFollowers.json";
+        public static string ExcludedNPCsPath { get; } = DataPath + PatcherName + "/excludedNPCs.json";
+        public static string ExcludedPerksPath { get; } = DataPath + PatcherName + "/excludedPerks.json";
+        public static string NPCEDIDPath { get; } = DataPath + PatcherName + "/NPCsByEDID.json";
+        public static string NPCFactionPath { get; } = DataPath + PatcherName + "/NPCsByFaction.json";
+        public static string RaceModifiersPath { get; } = DataPath + PatcherName + "/raceLevelModifiers.json";
+        public static string ZoneTyesEDIDPath { get; } = DataPath + PatcherName + "/zoneTypesByEDID.json";
+        public static string ZoneTyesEDIDMLUPath { get; } = DataPath + PatcherName + "/zoneTypesByEDIDMLU.json";
+        public static string ZoneTyesKeywordPath { get; } = DataPath + PatcherName + "/zoneTypesByKeyword.json";
+        public static string ZoneTyesKeywordMLUPath { get; } = DataPath + PatcherName + "/zoneTypesByKeywordMLU.json";
     }
 }
